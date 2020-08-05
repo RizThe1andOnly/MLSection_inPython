3959c7f5f09b1cbedf71581eb7a973b274f6d2af
Rizwan <risla156@gmail.com>
2020-08-05 12:09:03 -0400

    adding scikit learn mlr code

--- MLR.py
+++ MLR.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sklearn.linear_model
+import math
 
 
 NUMBER_OF_SAMPLES = 100
@@ -19,5 +20,59 @@ def runMultiNomialTraining(inputArray):
     pass
 
 
-randomArr = createRandomData()
-#runMultiNomialTraining(randomArr) #un-comment this command once runMultinomialTraining has been written
+#start of Iterative Reweighted Least Squares training algo; will have all of the sub-functions
+
+#obtaining fitted probabilities:
+
+def getExp_of_linearFunction(Bk,xi):
+    """
+        Will obtain: exp(Bk0 + (Bk)T * xi)
+
+        Bk = Row of coefficients; Bk0 is the first coeff; k corresponds to which outcome is being tested
+        xi = a sample
+    """
+    bk0 = Bk[0]
+    bk = np.array(Bk[:])
+    ans = bk0 + np.dot(bk,xi)
+    expAns = math.exp(ans)
+    return expAns
+
+def getExp_of_linerFuntion_Summation(B,x,K):
+    """
+        Will obtain: sum(1,k-1,exp(Bk0+(Bk)T * xi))
+
+        B = matrix of coeffs
+        x = feature vector or a sample row
+        K = number of features + 1. Note indices start from 0
+    """
+    summation = 0
+    for i in range(K):
+        summation = summation + getExp_of_linearFunction(B[i],x)
+    return summation
+
+def getProbabilityOf_outcome_given_features(B,xi,K,k):
+    """
+        Will obtain: ( exp(Bk0+(Bk)T*xi) / 1 + sum(1,k-1,exp(Bk0+(Bk)T*xi)) )
+
+        Outcomes are: k = 0...K-1
+
+        B = matrix of coeffs
+
+        xi = features input or a sample row
+
+        K = number of features + 1 ?
+
+        k = which outcome in particular
+    """
+    numerator = getExp_of_linearFunction(B[k],xi)
+    denominator = 1 + getExp_of_linerFuntion_Summation(B,xi,K)
+    ans =  numerator / denominator
+    return ans
+
+def getFittedProbabilityVector(B,X,K):
+    """
+        Obtains the vector of fitted probabilities where the ith elem is 
+        Pr(Y=k|X=xi;B)
+    """
+
+
